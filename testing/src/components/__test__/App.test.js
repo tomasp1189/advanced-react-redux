0a7db36b88c001a346a08b18ae55d660474cb870
Tomas <tomasp1189@gmail.com>
2018-08-07 09:55:10 -0300

    Setup Enzyme testing tools
    I setup the Enzyme tools and adapter, also included a  couple new tests.

---
+++ testing/src/components/__test__/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+it('should show a comment box', () => {
+	const wrapped = shallow(<App />);
+
+	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('should show a comment list', () => {
+	const wrapped = shallow(<App />);
+
+	expect(wrapped.find(CommentList).length).toEqual(1);
+});
